c97df12be0a38a1a762b4af266c320e5aeea3f75
Douglas Oliveira <douglas4030@gmail.com>
2022-07-14 19:20:57 -0300

    style: button's ajustments

--- lib/main.dart
+++ lib/main.dart
@@ -17,41 +17,51 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Calculadora de IMC'),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.refresh))],
-      ),
-      backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Icon(Icons.person_outline, size: 120, color: Colors.green),
-          const TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: 'Peso (Kg)',
-                  labelStyle: TextStyle(color: Colors.green)),
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.green, fontSize: 25)),
-          const TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: 'Altrura (Cm)',
-                labelStyle: TextStyle(color: Colors.green)),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.green, fontSize: 25),
+        appBar: AppBar(
+          title: const Text('Calculadora de IMC'),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.refresh))],
+        ),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.fromLTRB(10, 0, 0, 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(Icons.person_outline, size: 120, color: Colors.green),
+              const TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: 'Peso (Kg)',
+                      labelStyle: TextStyle(color: Colors.green)),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.green, fontSize: 25)),
+              const TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: 'Altrura (Cm)',
+                    labelStyle: TextStyle(color: Colors.green)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 25),
+              ),
+              Padding(
+                  padding: EdgeInsets.only(top: 10, bottom: 10),
+                  child: Container(
+                    height: 50,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('Calcular',
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 25))),
+                  )),
+              const Text(
+                'Info',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 25),
+              )
+            ],
           ),
-          Container(
-            height: 50,
-            child: RaisedButton(
-                onPressed: () {},
-                child: const Text('Calcular',
-                    style:  TextStyle(color: Colors.white, fontSize: 25))),
-          )
-        ],
-      ),
-    );
+        ));
   }
 }
